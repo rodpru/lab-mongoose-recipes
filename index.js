3b11f0935852b98ab4a7f068a56799c0a21045f5
rodpru <rgp.prt@gmail.com>
2020-11-05 11:49:06 +0000

    done

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ mongoose
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
-    return self.connection.dropDatabase();
+    // return self.connection.dropDatabase();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
@@ -26,14 +26,34 @@ mongoose
     console.error('Error connecting to the database', error);
   });
 
-  Recipe.create({
-    title: 'primeiro',
-    level: 'Easy Peasy',
-    cuisine: 'portuguesa',
-    ingridients: ['salt', 'sugar'],
-    dishType: 'snack',
-    duration: 10,
-    creator: 'MySelf',
-  }).then((res) =>{
-    console.log(`New recipie was just created ${res}`);
-  });
+  // Recipe.create({
+  //   title: 'primeiro',
+  //   level: 'Easy Peasy',
+  //   cuisine: 'portuguesa',
+  //   ingridients: ['salt', 'sugar'],
+  //   dishType: 'snack',
+  //   duration: 10,
+  //   creator: 'MySelf',
+  // }).then((res) =>{
+  //   console.log(`New recipie was just created ${res}`);
+  //    mongoose.connection.close();
+  // });
+
+//Recipe.insertMany(data);
+
+// Recipe.updateOne({title: 'Rigatoni alla Genovese'}, {duration: 100})
+// .then((updatedRigatoni) => {
+//   console.log(`The dish was updated ${updatedRigatoni`);
+//    mongoose.connection.close();
+// });
+// Recipe.find({}, 'title').then((result) =>{
+//   console.log(result);
+// })
+
+
+// Recipe.deleteOne({title: 'Carrot Cake'}).then((delCake) =>{
+//   console.log('Cake deleted');
+//    mongoose.connection.close();
+// });
+
+
